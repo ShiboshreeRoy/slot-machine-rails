a37c2c55e3e6e8d9406a98649cb1fa6841930760
ShiboshreeRoy <shiboshreeroy169@gmail.com>
2025-10-26 15:53:01 +0600

    Update gamesession Migration

--- db/migrate/20251026095123_create_game_sessions.rb
+++ db/migrate/20251026095123_create_game_sessions.rb
@@ -2,7 +2,7 @@ class CreateGameSessions < ActiveRecord::Migration[7.2]
   def change
     create_table :game_sessions do |t|
       t.references :user, null: false, foreign_key: true
-      t.integer :balance
+      t.integer :balance, default: 0
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_26_094903) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_26_095123) do
+  create_table "game_sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "balance", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_game_sessions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -22,4 +30,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_26_094903) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "game_sessions", "users"
 end
